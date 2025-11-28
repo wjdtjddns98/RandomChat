793237853bedc3da27b05c94ee015bfb2e91a7de
sungwoon <wjdtjddns98@naver.com>
2025-11-28 22:17:12 +0900

    # Commit Convention # ✨ feat     : 새로운 기능 추가 # 🐛 fix      : 버그 수정 # 💡 chore    : 기능 추가 없이 코드 수정 (오타, 주석 등) # 🎨 style    : 코드 포매팅 수정 # 📝 docs     : 문서 수정 (README 등) 🚚 build    : uv add uvicorn # ✅ test     : 테스트 코드 추가/변경 (프로덕션 코드 변경 없음) # ♻️ refactor : 리팩터링 (기능 변화 없음) # 🚑 hotfix   : 긴급 수정 # # 아래 1번 문항부터 주석 문구가 빈줄에 주석을 지우고 문항에 대한 내용을 작성하고 커밋을 완료해주세요. # # 1. 아래 형식에 맞춰 커밋 메시지 타이틀을 작성하세요: # <이모지> <타입>: <간결한 커밋 메시지 요약> # # 예시: # ✨ feat: 사용자 로그인 기능 추가 # 🐛 fix: 댓글 생성 시 발생하는 NullPointerException 수정 # 💡 chore: 불필요한 로그 제거 및 변수명 수정 # 🎨 style: black, isort 코드 포매터 실행 # 📝 docs: README에 프로젝트 설명 추가 # 🚚 build: Dockerfile 수정하여 실행 오류 해결 # ✅ test: 게시글 API 단위 테스트 추가 # ♻️ refactor: 중복 코드 제거 및 함수 분리 # 🚑 hotfix: 프로덕션 장애 수정 - 잘못된 URL 패턴 수정
    # 2. 변경 또는 추가사항을 아래에 간략하게 작성하세요 ( 필수 )
    #
    # 본문 내용은 어떻게 변경했는지 보다 무엇을 변경했는지 또는 왜 변경했는지를 설명합니다.
    
    # 3. 이슈가 있다면 아래에 연결하세요 ( 선택 )
    #
    # 예시
    # 관련 이슈: #123

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
 
+router = APIRouter()
+
+@router.get("/hello")
+async def hello_world():
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def hello_world(name: str):
+    return {"message": f"Hello {name}"}
